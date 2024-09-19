7ce62b26dfa831869424adea7c889c2d6f1a3950
abouelfaraj <156446000+abouelfaraj@users.noreply.github.com>
2024-09-19 12:22:38 +0200

    Delete quotes/Dashboard directory

--- quotes/Dashboard/views/dashboard.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-def index(request):
-    # context = get_current_language
-    return render(request, "adm/index.html")
